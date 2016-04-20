0b66e3d230250a267ca117bd2dde856042417ebb
sses79 <sses79@gmail.com>
2016-04-20 13:04:52 +0100

    Add Function makeMove() in Class Game and some testValidMove tests in GameTest

--- ChessProject-PHP/src/Game.php
+++ ChessProject-PHP/src/Game.php
@@ -26,41 +26,78 @@ class Game
         if(!$this->checkBoardBoundary($piece, $newX, $newY)){
             return FALSE;
         }
-        //if(!checkNotSameSquare($piece, $newX, $newY)){
-        //    return FALSE;
-        //}
-        //if(!$piece->move(MovementTypeEnum::MOVE(),$newX, $newY)){
-        //    return FALSE;
-        //}
-        //if(!checkNotOccupied($piece, $newX, $newY)){
-        //    return FALSE;
-        //}
-        return TRUE;
-    }
-
-    //private function checkNotOccupied(Piece $piece, $newX, $newY){
-    //    $currentPiece = self::getBoard()->getPiece($newX, $newY);
-    //
-    //    if($currentPiece == 0
-    //    || $currentPiece->getPieceColor() != $piece->getPieceColor())
-    //        return FALSE;
-    //}
-
-    private function checkBoardBoundary(Piece $piece, $newX, $newY){
-        if(!$this->_Board->isLegalBoardPosition($newX, $newY)){
+        if(!$this->checkNotSameCell($piece, $newX, $newY)){
             return FALSE;
         }
-        return TRUE;
+        if(!$piece->move(MovementTypeEnum::MOVE(),$newX, $newY)){
+            return FALSE;
+        }
+        if($this->checkNotOccupied($newX, $newY)){
+            return true;
+        }else{
+            if($this->checkOccupiedBySameColor($piece, $newX, $newY)){
+                return false;
+            }
+            return true;
+        }
+    }
+
+    private function checkBoardBoundary(Piece $piece, $newX, $newY){
+        if($this->_Board->isLegalBoardPosition($piece->getXCoordinate(), $piece->getYCoordinate())
+            && $this->_Board->isLegalBoardPosition($newX, $newY)){
+            return true;
+        }
+        return false;
+    }
+
+    private function checkNotSameCell(Piece $piece, $newX, $newY)
+    {
+        if($piece->getXCoordinate() !== $newX || $piece->getYCoordinate() !== $newY){
+            return true;
+        }
+        return false;
+    }
+
+    private function checkNotOccupied($newX, $newY){
+        if(is_null($this->_Board->getPiece($newX, $newY))){
+            return true;
+        }
+        return false;
+    }
+
+    private function checkOccupiedBySameColor(Piece $piece, $newX, $newY){
+        $current_piece = $this->_Board->getPiece($newX, $newY);
+        if($current_piece->getPieceColor() == $piece->getPieceColor()){
+            //Occupied by same color
+            return true;
+        }
+        //Occupied by different color
+        return false;
     }
 
     public function makeMove(Piece $piece, $newX, $newY){
         if($this->validateMove($piece, $newX, $newY)){
-
+            if($this->checkNotOccupied($newX, $newY) ){
+                $this->move($piece, $newX, $newY);
+            }
+            if(!$this->checkOccupiedBySameColor($piece, $newX, $newY)){
+                $this->capture($piece, $newX, $newY);
+            }
+        }else{
+            // TODO
+            // Notify this is not a valid move
         }
 
     }
 
+    private function move(Piece $Piece, $newX, $newY){
+        $this->_Board->movePiece($Piece, $newX, $newY);
+    }
 
+    private function capture(Piece $Piece, $newX, $newY){
+        $this->_Board->removePiece($newX, $newY);
+        $this->_Board->movePiece($Piece, $newX, $newY);
+    }
 
 
 }
--- ChessProject-PHP/tests/GameTest.php
+++ ChessProject-PHP/tests/GameTest.php
@@ -5,6 +5,7 @@ namespace LogicNow\Test;
 
 use LogicNow\Game;
 use LogicNow\Pawn;
+use LogicNow\Rook;
 use LogicNow\PieceColorEnum;
 
 class GameTest extends \PHPUnit_Framework_TestCase
@@ -18,11 +19,59 @@ class GameTest extends \PHPUnit_Framework_TestCase
         $this->_testSubject = new Game();
     }
 
-    public function testValid_Move()
+    public function testValidMove()
     {
         $firstPawn = new Pawn(PieceColorEnum::BLACK());
-        $test = $this->_testSubject->validateMove($firstPawn,5,5);
+        $this->_testSubject->getBoard()->add($firstPawn, 6, 3);
+        $test = $this->_testSubject->validateMove($firstPawn, 6, 2);
         $this->assertTrue($test);
+        $this->assertEquals(6, $firstPawn->getXCoordinate());
+        $this->assertEquals(2, $firstPawn->getYCoordinate());
+    }
+
+    public function testValidMove_Not_Same_Cell()
+    {
+        $firstPawn = new Pawn(PieceColorEnum::BLACK());
+        $this->_testSubject->getBoard()->add($firstPawn, 6, 3);
+        $test = $this->_testSubject->validateMove($firstPawn, 6, 3);
+        $this->assertFalse($test);
+    }
+
+    /**
+     *
+     */
+    public function testValidMove_Piece_Move_IllegalCoordinates_Left_DoesNotMove()
+    {
+        $firstPawn = new Pawn(PieceColorEnum::BLACK());
+        $this->_testSubject->getBoard()->add($firstPawn, 6, 3);
+        $test = $this->_testSubject->validateMove($firstPawn, 4, 3);
+        $this->assertFalse($test);
+    }
+
+    /**
+     *
+     */
+    public function testValidMove_Piece_Move_OccupiedBySameColor()
+    {
+        $firstPawn = new Pawn(PieceColorEnum::BLACK());
+        $firstRook = new Rook(PieceColorEnum::BLACK());
+        $this->_testSubject->getBoard()->add($firstPawn, 6, 3);
+        $this->_testSubject->getBoard()->add($firstRook, 6, 2);
+        $test = $this->_testSubject->validateMove($firstPawn, 6, 2);
+        $this->assertFalse($test);
+    }
+
+    /**
+     *
+     */
+    public function testMakeMove_Piece_Move_OccupiedByDifferentColor()
+    {
+        $firstPawn = new Pawn(PieceColorEnum::BLACK());
+        $firstRook = new Rook(PieceColorEnum::WHITE());
+        $this->_testSubject->getBoard()->add($firstPawn, 6, 3);
+        $this->_testSubject->getBoard()->add($firstRook, 6, 2);
+        $this->_testSubject->makeMove($firstPawn, 6, 2);
+        $this->assertEquals(2, $firstPawn->getYCoordinate());
     }
 
 }
