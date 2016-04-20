b9238e1f226efbea4dbb85cad686e97625b485c8
sses79 <sses79@gmail.com>
2016-04-20 12:10:53 +0100

    Refactor Pawn class: add a general class Piece of which Pawn is a subclass, and pass all existing tests.

--- ChessProject-PHP/src/Pawn.php
+++ ChessProject-PHP/src/Pawn.php
@@ -3,72 +3,19 @@
 namespace LogicNow;
 
 
-class Pawn
+class Pawn extends Piece
 {
-
-    /** @var PieceColorEnum */
-    private $_pieceColorEnum;
-    /** @var  ChessBoard */
-    private $_chessBoard;
-    /** @var  int */
-    private $_xCoordinate;
-    /** @var  int */
-    private $_yCoordinate;
 
     public function __construct(PieceColorEnum $pieceColorEnum)
     {
-        $this->_pieceColorEnum = $pieceColorEnum;
-    }
-
-    public function getChesssBoard()
-    {
-        return $this->_chessBoard;
-    }
-
-    public function setChessBoard(ChessBoard $chessBoard)
-    {
-        $this->_chessBoard = $chessBoard;
-    }
-
-    /** @return int */
-    public function getXCoordinate()
-    {
-        return $this->_xCoordinate;
-    }
-
-    /** @var int */
-    public function setXCoordinate($value)
-    {
-        $this->_xCoordinate = $value;
-    }
-
-    /** @return int */
-    public function getYCoordinate()
-    {
-        return $this->_yCoordinate;
-    }
-
-    /** @var int */
-    public function setYCoordinate($value)
-    {
-        $this->_yCoordinate = $value;
-    }
-
-    public function getPieceColor()
-    {
-        return $this->_pieceColorEnum;
-    }
-
-    public function setPieceColor(PieceColorEnum $value)
-    {
-        $this->_pieceColorEnum = $value;
+        parent::__construct($pieceColorEnum);
     }
 
     public function move(MovementTypeEnum $movementTypeEnum, $newX, $newY)
     {
         switch ($movementTypeEnum) {
             case MovementTypeEnum::MOVE():
-                switch ($this->_pieceColorEnum) {
+                switch ($this->getPieceColor()) {
                     case PieceColorEnum::WHITE():
                         //Pawn only allow change Y
                         if($newX == $this->getXCoordinate()){
@@ -100,23 +47,11 @@ class Pawn
                 break;
             case MovementTypeEnum::CAPTURE():
                 // TODO
+                return false;
                 break;
             default:
                 throw new \InvalidArgumentException("$movementTypeEnum is not a valid movement");
         }
-    }
-
-    public function toString()
-    {
-        return $this->currentPositionAsString();
-    }
-
-    protected function currentPositionAsString()
-    {
-        $result = "Current X: " . $this->_xCoordinate . PHP_EOL;
-        $result .= "Current Y: " . $this->_yCoordinate . PHP_EOL;
-        $result .= "Piece Color: " . $this->_pieceColorEnum;
-        return $result;
     }
 
 }
---
+++ ChessProject-PHP/src/Piece.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace LogicNow;
+
+
+abstract class Piece
+{
+
+    /** @var PieceColorEnum */
+    private $_pieceColorEnum;
+    /** @var  ChessBoard */
+    private $_chessBoard;
+    /** @var  int */
+    private $_xCoordinate;
+    /** @var  int */
+    private $_yCoordinate;
+
+    public function __construct(PieceColorEnum $pieceColorEnum)
+    {
+        $this->_pieceColorEnum = $pieceColorEnum;
+    }
+
+    public function getChessBoard()
+    {
+        return $this->_chessBoard;
+    }
+
+    public function setChessBoard(ChessBoard $chessBoard)
+    {
+        $this->_chessBoard = $chessBoard;
+    }
+
+    /** @return int */
+    public function getXCoordinate()
+    {
+        return $this->_xCoordinate;
+    }
+
+    /** @var int */
+    public function setXCoordinate($value)
+    {
+        $this->_xCoordinate = $value;
+    }
+
+    /** @return int */
+    public function getYCoordinate()
+    {
+        return $this->_yCoordinate;
+    }
+
+    /** @var int */
+    public function setYCoordinate($value)
+    {
+        $this->_yCoordinate = $value;
+    }
+
+    public function getPieceColor()
+    {
+        return $this->_pieceColorEnum;
+    }
+
+    public function setPieceColor(PieceColorEnum $value)
+    {
+        $this->_pieceColorEnum = $value;
+    }
+
+    abstract function move(MovementTypeEnum $movementTypeEnum, $newX, $newY);
+
+    public function toString()
+    {
+        return $this->currentPositionAsString();
+    }
+
+    protected function currentPositionAsString()
+    {
+        $result = "Current X: " . $this->_xCoordinate . PHP_EOL;
+        $result .= "Current Y: " . $this->_yCoordinate . PHP_EOL;
+        $result .= "Piece Color: " . $this->_pieceColorEnum;
+        return $result;
+    }
+
+}
